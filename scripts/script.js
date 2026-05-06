ddc099aacb051555d416957cca734cdb20d12432
Daniel Alves <da445689@gmail.com>
2026-05-06 00:12:11 -0300

    Cronometro concluido!

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,57 @@
-document.getElementById('relogio')
-let inicia = document.getElementById('inicia')
-let para = document.getElementById('para')
-let zera = document.getElementById('zera')
-let segundos = 0
-let min = 0
-let horas = 0
-function mostrar(){
-    relogio.innerHTML = `${String(horas).padStart(2,0)}:${String(min).padStart(2,0)}:${String(segundos).padStart(2,0)}`;
-};
+let relogio = document.getElementById("relogio");
+let inicia = document.getElementById("inicia");
+let para = document.getElementById("para");
+let zera = document.getElementById("zera");
+let segundos = 0;
+let min = 0;
+let horas = 0;
+let timer = 0;
+function mostrar() {
+  relogio.innerHTML = `${String(horas).padStart(2, 0)}:${String(min).padStart(2, 0)}:${String(segundos).padStart(2, 0)}`;
+}
 
-inicia.addEventListener('click', function(event){
-    timer = setInterval(function(){
-        segundos++
-        if(segundos == 60){
-            segundos = 0
-            min++
-        }
-        if(min == 60){
-            min = 0
-            horas++
-        }
-        mostrar()
-    },1000);
+let mudarCor = document.getElementById("mudarCor");
+mudarCor.addEventListener("click", function () {
+  document.body.classList.toggle("BodyWhite");
 });
 
-para.addEventListener('click', function(){
-    clearInterval(timer)
-    zera.style.display = 'flex'
-    para.style.display = 'none'
-    if(inicia === 'true'){
-        zera.style.display
+inicia.addEventListener("click", function (event) {
+  clearInterval(timer);
+  alterarBotao();
+  timer = setInterval(function () {
+    segundos++;
+    if (segundos == 60) {
+      segundos = 0;
+      min++;
     }
+
+inicia.addEventListener("click", function (event) {
+  clearInterval(timer);
+  zera.style.display = "none";
+  para.style.display = "flex";
+  timer = setInterval(function () {
+    segundos++;
+    if (segundos == 60) {
+      segundos = 0;
+      min++;
+    }
+    if (min == 60) {
+      min = 0;
+      horas++;
+    }
+    mostrar();
+  }, 1000);
 });
 
-zera.addEventListener('click', function(){
-    segundos = 0
-    min = 0
-    horas = 0
-    mostrar('')
+para.addEventListener("click", function () {
+  clearInterval(timer);
+  zera.style.display = "flex";
+  para.style.display = "none";
+});
+
+zera.addEventListener("click", function () {
+  segundos = 0;
+  min = 0;
+  horas = 0;
+  mostrar();
 });
